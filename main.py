e9cc97d097198422a3a4adf172f14cf92f34665f
achatman <andrew.chatman9@gmail.com>
2018-10-28 23:17:27 -0500

    Started frequency analysis.

--- main.py
+++ main.py
@@ -99,6 +99,48 @@ class Cipher:
             for k, v in self.mapping.items():
                 outfile.write(f'{k}:{v}\n')
 
+    def load_frequency(self, path):
+        freq = dict()
+        with open(path) as infile:
+            for line in infile:
+                k, v = line.strip().split(':')
+                freq.update({k: float(v)})
+        return freq
+
+    def frequency_analysis(self, encoded, freq_path, map_path='map.txt'):
+        if self.charset == None:
+            raise CipherException('Charset must be defined before running a frequency analysis.')
+        ideal_freq = self.load_frequency(freq_path)
+        real_freq = dict()
+        for char in self.charset:
+            if char in self.charset:
+                real_freq.update({char: encoded.count(char)})
+        total = sum([v for k,v in real_freq.items()])
+        for char in real_freq:
+            real_freq[char] = 100 * real_freq[char] / total
+        sorted_ideal = sorted(ideal_freq.items(), key=lambda kv: kv[1])
+        sorted_real = sorted(real_freq.items(), key=lambda kv: kv[1])
+        print(sorted_ideal)
+        print()
+        print(sorted_real)
+        print()
+        self.mapping = dict()
+        for i in range(len(sorted_ideal)):
+            self.mapping.update({sorted_real[i][0]: sorted_ideal[i][0]})
+            print(sorted_real[i], sorted_ideal[i])
+        with open(map_path, 'w') as outfile:
+            for k, v in self.mapping.items():
+                outfile.write(f'{k}:{v}\n')
+
 
 class CipherException(Exception):
     pass
+
+
+enc = '''jhwabylk zopw av wpyhal ihzl: dl ohcl jhwabylk h zjpluapmpj clzzls pu aol dhalyz ulhy aol svuzkhsl yllm.  aolf hyl zabkfpun zvtl uld zwljplz vm mpzo.  p dvukly pm aolf jhbnoa hufaopun klspjpvbz?
+wpyhal ihzl av jhwabylk zopw: nvvk dvyr!  kpylja aol jvbyzl vm fvby zopw avdhykz tlpauly pzshuk huk wpjr bw zvtl jvjvubaz aolyl mvy kpuuly avupnoa.  adluaf vm aolt zovbsk il luvbno.
+jhwabylk zopw av wpyhal ihzl: dl nva svza olhkpun av tlpauly pzshuk huk dlua aol vwwvzpal kpyljapvu puzalhk, av ovwwly pzshuk.  hdhpapun mbyaoly puzaybjapvuz.  dl hszv zhd h nyvbw vm dohslz.  aol zjpluapzaz zhpk pa pz jhsslk h ‘wvk.’'''
+c = Cipher()
+c.generate_charset('alpha_low')
+c.frequency_analysis(enc, 'frequencies/english.freq')
+print(c.decode(enc))
